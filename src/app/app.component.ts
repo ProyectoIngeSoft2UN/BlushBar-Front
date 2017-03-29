a7daccf060c7fe21cede8faef4efe8facf0e871c
JuanDavid Rojas <jdrojass@unal.edu.co>
2017-03-29 07:42:13 -0500

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { navbarComponent } from './navbar/app.navbarComponent';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { navbarComponent } from './navbar/app.navbarComponent';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, navbarComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +16,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, navbarComponent]
 })
 export class AppModule { }
