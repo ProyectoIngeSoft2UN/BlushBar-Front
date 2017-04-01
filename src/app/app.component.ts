9dcd7cd2d7eb9dc75554d76e0a590c849f416982
JuanDavid Rojas <jdrojass@unal.edu.co>
2017-04-01 18:01:36 -0500

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { navbarComponent } from './navbar/app.navbarComponent';
 import { headerComponent } from './header/app.headerComponent';
 import { blockComponent } from './header/block/app.blockComponent';
-import { footerComponent } from './footer/app.footerComponent';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/app.router.ts
@@ -0,0 +1,14 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
+
+import { AppComponent } from './app.component'
+import { InfoComponent } from './info/info.component'
+import { HomeComponent } from './home/home.component'
+
+export const router: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'info', component: InfoComponent },
+  { path: 'home', component: HomeComponent }
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(router);
