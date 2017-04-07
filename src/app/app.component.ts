59693a33a1af33643c185a38c6d5a8669b76a7ef
JuanDavid Rojas <jdrojass@unal.edu.co>
2017-04-07 13:54:53 -0500

    enable rails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { navbarComponent } from './navbar/app.navbarComponent';
 import { headerComponent } from './header/app.headerComponent';
 import { blockComponent } from './header/block/app.blockComponent';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,9 @@ import { blockComponent } from './header/block/app.blockComponent';
 })
 export class AppComponent {
   title = 'app works finaly!';
+  product;
+
+  constructor(private http: Http) {
+    http.get('http://localhost:3000/product.json').subscribe(res => this.product = res.json());
+  }  
 }
