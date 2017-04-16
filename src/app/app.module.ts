a78d896be4755e18e8fb1f5d77b09b3e8db8b1bc
Juliana <jucninova@unal.edu.co>
2017-04-16 10:30:01 -0500

    Creación de nuevas vistas: Skin Care, Body Care, Fragances.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,54 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+  import { BrowserModule } from '@angular/platform-browser';
+  import { NgModule } from '@angular/core';
+  import { FormsModule } from '@angular/forms';
+  import { HttpModule } from '@angular/http';
 
-import { routes } from './app.router'
+  import { routes } from './app.router';
 
-import { AppComponent } from './app.component';
-import { navbarComponent } from './navbar/app.navbarComponent';
-import { headerComponent } from './header/app.headerComponent';
-import { blockComponent } from './header/block/app.blockComponent';
-import { InfoComponent } from './info/info.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { BodyComponent } from './home/body/body.component';
-import { MakeUpComponent } from './make-up/make-up.component';
-import { AccessoriesComponent } from './accessories/accessories.component';
-import { ProductComponent } from './product/product.component';
-import { ProductService } from './product/product.service';
-import { ServiceComponent } from './service/service.component'
+  import { AppComponent } from './app.component';
+  import { navbarComponent } from './navbar/app.navbarComponent';
+  import { headerComponent } from './header/app.headerComponent';
+  import { blockComponent } from './header/block/app.blockComponent';
+  import { InfoComponent } from './info/info.component';
+  import { FooterComponent } from './footer/footer.component';
+  import { HomeComponent } from './home/home.component';
+  import { BodyComponent } from './home/body/body.component';
+  import { MakeUpComponent } from './make-up/make-up.component';
+  import { SkinCareComponent } from './skin-care/skin-care.component';
+  import { BodyCareComponent } from './body-care/body-care.component';
+  import { FragancesComponent } from './fragances/fragances.component';
+  import { AccessoriesComponent } from './accessories/accessories.component';
+  import { ProductComponent } from './product/product.component';
+  import { ProductService } from './product/product.service';
+  import { ServiceComponent } from './service/service.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    navbarComponent,
-    headerComponent,
-    blockComponent,
-    InfoComponent,
-    FooterComponent,
-    HomeComponent,
-    BodyComponent,
-    MakeUpComponent,
-    AccessoriesComponent,
-    ProductComponent,
-    ServiceComponent
-  ],
+
+  @NgModule({
+    declarations: [
+      AppComponent,
+      navbarComponent,
+      headerComponent,
+      blockComponent,
+      InfoComponent,
+      FooterComponent,
+      HomeComponent,
+      BodyComponent,
+      MakeUpComponent,
+      SkinCareComponent,
+      BodyCareComponent,
+      FragancesComponent,
+      AccessoriesComponent,
+      ProductComponent,
+      ServiceComponent
+    ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     routes
   ],
+
+
   providers: [
     ProductService
   ],
---
+++ src/app/fragances/fragances.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FragancesComponent } from './fragances.component';
+
+describe('FragancesComponent', () => {
+  let component: FragancesComponent;
+  let fixture: ComponentFixture<FragancesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FragancesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FragancesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
