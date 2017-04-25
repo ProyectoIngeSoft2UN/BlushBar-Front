b94faf547588b464ffb619c68c82eeaab8dd98d7
JuanDavid Rojas <jdrojass@unal.edu.co>
2017-04-25 17:25:03 -0500

    commit update back

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -1,7 +1,9 @@
 export class Product {
   constructor(
-    public id?: number,
     public name?: string,
-    public description?: string
+    public costo?: number,
+    public subcategory_id?: string,
+    public image_id?: number,
+    public brand?: string
   ) {}
 }
